e441a0be247f566541d264695ded3727dd92d492
suhrob <suxrobamirov00@gmail.com>
2024-12-13 04:51:36 +0500

    yulandi

---
+++ js/app.js
@@ -0,0 +1,106 @@
+///////////////////////////////////////////  1  M  /////////
+
+///////////////////////////////////////////  2  M  /////////
+
+// const arr = [4, -5, 8, -6, -7, 3, 9, 8, 7, -5, 4, -1, 8, 9, 4, 7, -6];
+// const arr1 = [];
+// let arrLen = arr.length;
+
+// for (let i = 0; i < arrLen; i++) {
+//     arr1.push(Math.abs(arr[i]));
+// }
+
+// console.log(arr1);
+
+
+
+///////////////////////////////////////////  3  M  /////////
+
+// const arr = [4, -5, 8, -6, -7, 3, 9, 8, 7, -5, 4, -1, 8, 9, 4, 7, -6];
+// const arr1 = [];
+// let arrLen = arr.length;
+
+// for (let i = 0; i < arrLen; i++) {
+//     arr1.push(Math.pow(Math.abs(arr[i]), 2));
+// }
+
+// console.log(arr1);
+
+
+
+///////////////////////////////////////////  4  M  /////////
+
+// const arr = [4, -5, 8, -6, -7, 3, 9, 8, 7, -5, 4, -1, 8, 9, 4, 7, -6];
+// const arr1 = [];
+// let arrLen = arr.length;
+
+// for (let i = 0; i < arrLen; i++) {
+//     if(arr1.includes(arr[i]) == true){
+//        true;
+//     }else {
+//         arr1.push(arr[i]);
+//     }
+// }
+
+// console.log(arr1);
+
+
+
+///////////////////////////////////////////  5  M  /////////
+
+
+// let son1 = parseInt(prompt("soni kirting: "));
+// const arr = [4, -5, 8, -6, -7, 3, 9, 8, 7, -5, 4, -1, 8, 9, 4, 7, -6];
+// let son = 0;
+// let arrLen = arr.length;
+
+// for (let i = 0; i < arrLen; i++) {
+//     if (arr[i] % son1 != 0) {
+//         son = son + 1;
+//     }
+// }
+
+// console.log(son);
+
+
+
+///////////////////////////////////////////  6  M  /////////
+
+
+
+///////////////////////////////////////////  7  M  /////////
+
+// const arr = [4, -5, 8, -6, -7, 3, 9, 8, 7, -5, 4, -1, 8, 9, 4, 7, -6];
+// let son = -Infinity;
+// let arrLen = arr.length;
+
+// for(let i = 0 ; i <arrLen; i++){
+//     if(son < arr[i]){
+//         son = arr[i];
+//     }
+// }
+
+// while(arr.includes(son)){
+//     const index = arr.indexOf(son);
+//     arr.splice(index, 1);
+// }
+
+// console.log(arr);
+
+
+
+///////////////////////////////////////////  8  M  /////////
+
+// const arr = [ 4, 8, 5, -6, -2, 7, 5, 6, -5, -3, 6, 9, 7, 1, -5, -6, -2, 2, 3];
+// let arr1 = []
+// let arrLen = arr.length;
+
+// for(let i = arrLen-1 ; i>=0; i--){
+//     arr1.push(arr[i]);
+// }
+
+// console.log(arr1);
+
+
+
+///////////////////////////////////////////  9  M  /////////
